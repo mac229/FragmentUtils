c1124cc24fcd26eb8c22e6d92e55a1154c2a9cb6
Maciej Kozłowski <>
2018-04-08 14:11:57 +0200

    Added java fragment utils

---
+++ fragmentutils/src/main/java/com/maciejkozlowski/fragmentutils/FragmentUtils.java
@@ -0,0 +1,80 @@
+package com.maciejkozlowski.fragmentutils;
+
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+
+/**
+ * Created by Maciej Kozłowski on 08.04.2018.
+ */
+public final class FragmentUtils {
+
+    private FragmentUtils() {
+
+    }
+
+    @NonNull
+    public static <T> T getListenerOrThrowException(Fragment fragment, Class<T> listenerClazz) {
+        T listener = getListener(fragment, listenerClazz);
+        if (listener == null) {
+            throw new IllegalStateException("Not found " + listenerClazz.getSimpleName());
+        }
+
+        return listener;
+    }
+
+    @Nullable
+    public static <T> T getListener(Fragment fragment, Class<T> listenerClazz) {
+        T listener = getListenerFromTargetFragment(fragment, listenerClazz);
+        if (listener != null) {
+            return listener;
+        }
+
+        listener = getListenerFromParentFragment(fragment, listenerClazz);
+        if (listener != null) {
+            return listener;
+        }
+
+        listener = getListenerFromActivity(fragment, listenerClazz);
+        if (listener != null) {
+            return listener;
+        }
+
+        return null;
+    }
+
+    @Nullable
+    private static <T> T getListener(Class<T> listenerClass, Object object) {
+        if (listenerClass.isInstance(object)) {
+            return listenerClass.cast(object);
+        } else {
+            return null;
+        }
+    }
+
+    @Nullable
+    public static <T> T getListenerFromTargetFragment(Fragment fragment, Class<T> listenerClazz) {
+        return getListener(listenerClazz, fragment.getTargetFragment());
+    }
+
+    @Nullable
+    public static <T> T getListenerFromParentFragment(Fragment fragment, Class<T> listenerClazz) {
+        return getListener(listenerClazz, fragment.getParentFragment());
+    }
+
+    @Nullable
+    public static <T> T getListenerFromActivity(Fragment fragment, Class<T> listenerClazz) {
+        return getListener(listenerClazz, fragment.getActivity());
+    }
+
+    @Nullable
+    public static <T> T findFragmentByTag(FragmentManager fragmentManager, Class<T> fragmentClass, String tag) {
+        Fragment fragment = fragmentManager.findFragmentByTag(tag);
+        if (fragmentClass.isInstance(fragment)) {
+            return fragmentClass.cast(fragment);
+        } else {
+            return null;
+        }
+    }
+}
